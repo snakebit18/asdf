e04f846ea66a8946993638bd9146e371f9e36436
snakebit18 <sanketbandgar01@gmail.com>
2023-05-11 01:17:07 +0530

    commit2

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import awsExports from './src/aws-exports';
+import { withAuthenticator } from "aws-amplify-react-native";
+Amplify.configure(awsExports);
 
-export default function App() {
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -18,3 +23,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(app);
